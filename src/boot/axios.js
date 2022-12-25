45152bc660386391c43a5603ffc70d5c5ecb2ec2
tarnoma <p_jare@hotmail.com>
2022-12-25 15:13:52 +0700

    mod axios

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -3,7 +3,7 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'https://vercel.com/tarnoma/api4linenotif',
+const api = axios.create({ baseURL: 'https://api4linenotif.vercel.app',
 headers:{
     "Content-type": "application/json"
 } })
